c37f244b43de6aa15c796835c7255f8d4b94a5a9
Nancy <nancytochi24@gmail.com>
2021-03-02 12:19:50 -0500

    add more-work page

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const port = process.env.PORT || process.env.IP || 3000;
 
 const app = express();
 
-
+// View engine setup
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
+
+
+// Routes
 app.get("/", (req, res) => {
     res.render("home")
 });
@@ -15,4 +18,11 @@ app.get("/about", (req, res) => {
     res.render("about")
 });
 
-app.listen(port, () => console.log("Dorema Spire Has Started!"));
+app.get("/more-work", (req, res) => {
+    res.render("more-work")
+});
+
+
+app.listen(port, () => {
+    console.log('Dorema Spire Has Started');
+});
